e0a7da86fe7322896f80732c68592d81d6fc0af2
HaykZiroyan <haik.ziroyan@mail.ru>
2022-02-18 23:15:47 +0400

    last

--- js-task/5/main.js
+++ js-task/5/main.js
@@ -1,81 +1,86 @@
-    var list = [
-        {
-            name: "item 1"
-        },
-        {
-            name: "item 2"
-        },
-        {
-            name: "item 3"
-        },
-        {
-            name: "item 4",
-            items: [
-                {
-                    name: "sub-item 1"
-                },
-                {
-                    name: "sub-item 2"
-                },
-                {
-                    name:  "sub-item 3",
-                    items: [
-                        {
-                            name: "sub-sub-item 1"
-                        },
-                        {
-                            name: "sub-sub-item 2"
-                        },
-                        {
-                            name:  "sub-sub-item 3",
-                            items: [
-                                {
-                                    name: "sub-sub-sub-item 1"
-                                },
-                                {
-                                    name: "sub-sub-sub-item 2"
-                                },
-                                {
-                                    name: "sub-sub-sub-item 3"
-                                },
-                                {
-                                    name: "sub-sub-sub-item 4"
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    name:  "sub-item 4"
-                }
-            ]
-        },
-        {
-            name: "item 5"
-        },
-        {
-            name: "item 6"
-        }
-    ]
+var list = [
+    {
+        name: "item 1"
+    },
+    {
+        name: "item 2"
+    },
+    {
+        name: "item 3"
+    },
+    {
+        name: "item 4",
+        items: [
+            {
+                name: "sub-item 1"
+            },
+            {
+                name: "sub-item 2"
+            },
+            {
+                name:  "sub-item 3",
+                items: [
+                    {
+                        name: "sub-sub-item 1"
+                    },
+                    {
+                        name: "sub-sub-item 2"
+                    },
+                    {
+                        name:  "sub-sub-item 3",
+                        items: [
+                            {
+                                name: "sub-sub-sub-item 1"
+                            },
+                            {
+                                name: "sub-sub-sub-item 2"
+                            },
+                            {
+                                name: "sub-sub-sub-item 3"
+                            },
+                            {
+                                name: "sub-sub-sub-item 4"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name:  "sub-item 4"
+            }
+        ]
+    },
+    {
+        name: "item 5"
+    },
+    {
+        name: "item 6"
+    }
+]
 
-    var div = document.getElementById('add');
+var div = document.getElementById('add');
+var div1 = document.getElementById('demoC');
+function create(where, from) {
+    var ul = document.createElement("ul");
+    for(var i = 0; i<from.length; i++) {
+        if (Object.keys(from[i]).length == 1) {
+            let li = document.createElement("li");
+            li.innerText = from[i][Object.keys(from[i])];
+            ul.appendChild(li);
+        } else {
+            let li = document.createElement("li");
+            var uls = document.createElement("ul");
 
-    
-    list.forEach(element => {
-        let li = document.createElement("li");
-        li.innerText = element[Object.keys(element)];
-        if (element[Object.keys(element)] == undefined) {
-            element.forEach(elem => {
-                console.log(elem[Object.keys(elem)])
-            })
-            // li.innerText = element[Object.keys(element)];
+            // var ul1 = document.createElement("ul");
+            li.innerText = from[i][Object.keys(from[i])[0]];
+            li.appendChild(uls);
+            ul.appendChild(li);
+            
+            create(uls, from[i][Object.keys(from[i])[1]]);
         }
-        div.appendChild(li);
-    });
-    // }
-    // adds(list, div);
-    // list.forEach(element => {
-    //     let li = document.createElement("li");
-    //     li.innerText = element[Object.keys(element)] ;
-    //     div.appendChild(li);
-    // });
+
+
+    }
+    where.appendChild(ul);
+}
+create(div,list);
--- math-test/4/main.js
+++ math-test/4/main.js
@@ -1,38 +1,3 @@
-// function isOperand(c){
-//     return (c.charCodeAt(0) >= '0'.charCodeAt(0) && c.charCodeAt(0) <= '9'.charCodeAt(0));
-// }
- 
-// function value(c){
-//     return (c.charCodeAt(0) - '0'.charCodeAt(0));
-// }
- 
-// function evaluate(exp) {
-//     if (exp.length == 0) return -1;
-//     let res = value(exp[0]);
-   
-//     for (let i = 1; i<exp.length; i += 2){
-//         let opr = exp[i], opd = exp[i+1];
-   
-//         if (isOperand(opd) == false) return -1;
-   
-//         if (opr == '+') res += value(opd);
-//         else if (opr == '-') res -= value(opd);
-//         else if (opr == '*') res *= value(opd);
-//         else if (opr == '/') res /= value(opd);
-   
-//         else                 return -1;
-//     }
-//     return res;
-// }
-// function def(str) {
-//     const obj = str.split("=");
-//     return obj
-// }
-// var exp = def('2+2=a');
-// let ress = evaluate(exp[1]);
-// if(ress == -1) document.write(expr1+" is Invalid<br>");
-// else     document.write("Value of "+exp+" is "+ress+"<br>");
-
 var check = '1+7=3';
 var answear = true;
 var oporetions = '+-*/=';
